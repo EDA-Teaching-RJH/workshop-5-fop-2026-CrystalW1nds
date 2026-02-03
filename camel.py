0b47c693777eeaf3478d93e5141bcdcad07b7284
CrystalW1nds <hf280@kent.ac.uk>
2026-02-03 12:24:36 +0000

    part 1 finished

--- camel.py
+++ camel.py
@@ -1,1 +1,13 @@
-# Enter your code here
+def main():
+    fullWord = input("Please enter a full word to convert to snake case: ")
+    snakeWord = ""
+
+    for char in fullWord:
+        if char.isupper():
+            snakeWord += "_" + char.lower()
+        else:
+            snakeWord += char
+
+    print(snakeWord)
+
+main()
