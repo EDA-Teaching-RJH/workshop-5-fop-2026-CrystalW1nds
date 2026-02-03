30f4fec1a578762ec67866a0ded738294f355e80
CrystalW1nds <hf280@kent.ac.uk>
2026-02-03 12:34:49 +0000

    part 2 finished

--- coffee.py
+++ coffee.py
@@ -1,1 +1,23 @@
-#Your code goes here
+def main():
+    balance = 0
+
+    while balance < 75:
+        coin = int(input("Insert 75p: (50p, 20p, 10p, 5p)"))
+
+        if coin == 50 or coin == 20 or coin == 10 or coin == 5:
+            balance = balance + coin
+        
+        else:
+            print("Invalid coin, refunding... ")
+
+        print("Balance: ", balance)
+
+    change = balance - 75
+
+    print("Change: ", change)
+    print("Dispensing change... ")
+
+    print("Preparing coffee...")
+
+
+main()
